fea2350f1135180bf03a52841907266456bbdf51
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-05-20 18:18:48 -0400

    mvp met

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie(props) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
+  const { id } = useParams();
 
   const fetchMovie = (id) => {
     axios
@@ -15,7 +17,7 @@ function Movie({ addToSavedList }) {
   };
 
   const saveMovie = () => {
-    addToSavedList(movie);
+    props.addToSavedList(movie);
   };
 
   useEffect(() => {
@@ -26,6 +28,20 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+  const deleteItem = (e) => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then((res) => {
+        // res.data
+        console.log(res);
+        // props.setItems(res.data);
+        push("/");
+        const newMovies = props.MovieList.filter((v) => `${v.id}` !== res.data);
+        props.setMovieList(newMovies);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
@@ -33,6 +49,15 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button
+        onClick={() => {
+          push(`/updated-movie/${id}`);
+        }}
+      >
+        {" "}
+        Update Movie
+      </button>
+      <button onClick={() => deleteItem()}> Delete Movie </button>
     </div>
   );
 }
